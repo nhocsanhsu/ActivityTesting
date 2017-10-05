aca4b791b6f90f66299021295da570b0bb766c5d
Huỳnh Trương Nhựt <luminous997@gmail.com>
2017-10-05 11:59:23 +0700

    abc

--- app/src/main/java/com/example/admin/activitytesting/MainActivity.java
+++ app/src/main/java/com/example/admin/activitytesting/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e(TAG,"onCreate");
+        Log.e(TAG,"onCreateNow");
     }
 
     @Override
